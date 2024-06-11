75690d6f33dfce34b32ef8dd70944146a57b2cee
agkayster <ejike.chiboka@gmail.com>
2024-06-11 13:25:39 +0100

    Installing and setting up shadcn/ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
+import Pay from './components/Pay';
 
 function App() {
-	return (
-		<h1 className='text-3xl text-red-500 font-bold underline'>
-			Hello world!
-		</h1>
-	);
+	return <Pay />;
 }
 
 export default App;
